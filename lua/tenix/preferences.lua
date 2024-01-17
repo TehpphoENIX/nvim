9dd03276840742c85fb4486040807f9bba35349e
Teh_phoENIX <kirill.zaritskiy@gmail.com>
2024-01-18 00:22:59 +0200

    Added more prefferences

--- lua/tenix/preferences.lua
+++ lua/tenix/preferences.lua
@@ -1,2 +1,22 @@
 vim.wo.relativenumber = true
 vim.wo.number = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
